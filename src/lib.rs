cf96ab655b6354f6a88af1bae1de4fcb39330d65
Saha <dwaipayan.saha@sap.com>
2018-08-26 20:31:35 +0530

    Set up basic routing

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,32 @@
 //! A Hello World example application for working with Gotham.
 
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
-
-const HELLO_WORLD: &'static str = "Hello World!";
-
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, HELLO_WORLD)))
-}
+use iron::{Iron, IronResult, Request, Response};
+use router::Router;
 
 /// Start a server and call the `Handler` we've defined above for each `Request` we receive.
 pub fn run() {
-    Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    let mut router = Router::new();
+    router.get("/", handler, "handler");
+    router.get("/:query", query_handler, "query_handler");
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn handler(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "OK")))
+    }
+
+    fn query_handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req
+            .extensions
+            .get::<Router>()
+            .unwrap()
+            .find("query")
+            .unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
+    }
 }
